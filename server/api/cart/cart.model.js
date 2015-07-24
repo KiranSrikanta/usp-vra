f6d5298b6c4a73ddce2bf3007e482d72d8bd964e
Kiran.Srikanta@emc.com <ravi_kiran_vs@yahoo.co.in>
2015-07-24 11:50:35 +0530

    changes to the oredr and cart schema

--- server/api/cart/cart.model.js
+++ server/api/cart/cart.model.js
@@ -5,12 +5,19 @@ var mongoose = require('mongoose'),
 
 var ItemSchema = new Schema({
   Id: String,
+  Title: String,
   Qty: { type: Number, min: 0 },
-  Price: { type: Number, min: 0 }
+  Price1: { type: Number, min: 0 },
+  Capacity1: String,
+  Price2: { type: Number, min: 0 },
+  Capacity2: String,
+  DefaultImage: String
 });
 
 var CartSchema = new Schema({
   UserName: String,
+  SubTotal: String,
+  ExpectedDelivery: String,
   Item: [ItemSchema]
 });
 
--- server/api/order/order.model.js
+++ server/api/order/order.model.js
@@ -3,13 +3,27 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var OrderedItemSchema = new Schema({
+	Title: String,
+	Price: { type: Number, min: 0 },
+	Quantity: { type: Number, min: 0 },
+	Total: { type: Number, min: 0 },
+	EstimatedDelivery: String,
+	Item: [Schema.Types.ObjectId]
+})
+
 var OrderSchema = new Schema({
+
+  CreatedOn: Date,
   RequestedBy: String,
   RequestedFor: String,
   Phone: String,
   Mobile: String,
-  Items: [Schema.Types.ObjectId],
-  Status: String
+  Status: String,
+  ManagerialApprover: String,
+  FinancialApprover: String,
+  BillingCostCenter: String,
+  Items: [OrderedItemSchema]
 });
 
 module.exports = mongoose.model('Order', OrderSchema);
